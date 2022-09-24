1a96a184044fd55ec561d5133bca1c3b4913c806
Atulit519 <77721130+Atulit519@users.noreply.github.com>
2022-09-24 17:02:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//https://teachablemachine.withgoogle.com/models/yaJkRdrBL/
+Webcam.set({
+    width: 300,
+    height: 300,
+    image_format: "png",
+    png_quality: 90
+});
+
+Webcam.attach("#camera");
